d0a2fb8d3546cf0e7d4b2c933708b250b556df94
c1c2c3c4c5c <151120869+c1c2c3c4c5c@users.noreply.github.com>
2024-10-20 23:11:35 +0530

    Update Create.js

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -6,7 +6,7 @@ const Create = () => {
     const [task, setTask] = useState('');
 
     const createTask = () => {
-        axios.post('http://localhost:5000/add', { task: task.trim() })
+        axios.post('https://kaushalam-niti-todo-app.onrender.com/add', { task: task.trim() })
             .then(result => {
                 console.log(result.data);
                 window.location.reload();
